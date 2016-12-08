62b908c7c1e1568fec7d1b342b570cb157e7a7e9
shoponpon <shoponprog@gmail.com>
2016-12-08 22:40:26 +0900

    first commit

---
+++ quine_fran.rb
@@ -0,0 +1,47 @@
+                                          eval$s=%w'bin="BAhsKw                                     
+                                       IkA                     QAAA                                 
+                                   AA                               A/P                             
+                                 9                                     /A                           
+                              AA                                    A   AAAA                        
+                            AA                                       AA       OAAA                  
+                           e            A                             AA          A                 
+                         A             A                               AA         A                 
+                       AA        GA    A            A    A              H        AA                 
+                      A  A       A   AAA            A    A               I       A                  
+                    AA  A       AA   YAA           AA    A                A     AA                  
+                   wA  A       AA   AAQD           wA   AA                 A   AA A                 
+                  A  MA        AA   AABj          wAA AAAAA                g  AAQA A                
+                 w AAEA       AAA  ACAAC          AAA   ABh   AA            AAAAIABhgA              
+               QAgA  B B     gAAA AA kIA4         A IQ Ag I   A   AAAA      AwA eMA  GA             
+              IA    B  A    MAAA  CA  CRg        Pg DE Ag Fg AA   AAA       ZID xAN                 
+                    w HA    Mg L AA    AAo       A   ecD   8 BxD  A D       /A  wAA                 
+                    g Kf   g7Q G0RT      xw   Z wA   AAA   wJzxx g  2 8Y   Bd   wAA                 
+                    AADQ   sIY  D4    ug  UMA cAA   AAAjXtgNI4vj    X EA        AA                  
+                    AA8Dj  D 3  PH/0AAD      AA               AA    AJ/y        Bw                  
+                    YADA8 wA AAAA                      P D t  AQ    CAyv       CA                   
+                    AQA   A AA   B H o                w I A   VA    wPHA      AAA                   
+             OD/EBUAAMBg    4   A E A                         AI     AD      PgOA                   
+           /wA       PBw8A  AA             CMz/MAAIB        /aJk    AAA     DADP                    
+          P5   //8fV  08AA  AAAT                       Bgw2/+D/    7 sD A  AA  A                    
+          wM  DA    zv  4Q/OM  YAAAAABgABofEPXyDAQAAAAAHY   wBs / m AGAA  A                         
+          AA  A    Dg       gA  Ng Ng 5jAAAAAAAA+D     cA5+EYAuAAA AAA AMA                          
+          OP      B7      ec ID/  AA AAAACH4    e     HBBw/+/w   8AA   AA                           
+           eM            Ab     8f7    D  x   /38 AAAD    8P/6A 4w     P4                           
+            Pz4     +A   AB           +f O7  /Pxng/z     Ez  Bg                                     
+             AgZ       gz/           Qw  Pv x2   ccP    gA   Af                                     
+               HQ+fgQg4 48          JAA  AAAA    Agk   sU    A           s4v                        
+                      4A AAA      AAAA   GM4c    Hm8G AA    AAA        AAAAHwMP1                    
+                    k8A    AA    AAAA    AADD     AvAkD     HAAA     AAAAAOAAuMg/AAIAAAA            
+                 AAIA       NA   AA       wAHgAAAAAAAD4     // 9/   AOAMAAAAAAAAAOAD+P8             
+              z";n=Marshal   .load(b       in.   unpac         k("m")[0]);   e="ev   al$s=          
+         %w"<<3   9<<($  s*3 );o="";j=-1;0.upt  o  (4        7*100-1){|i|   ;o  <<  ((   n[         
+         i   ]=  =1   )?    e[j+=1]:32    ) ;o      <<(( i%100=   =99)?  10   :""    );};o          
+          [-10,   6 ]="  "<<39   <<".jo   i          n  ";   puts(o)   #b  i                        
+                         n   =  "  BA h   sK        wI  kA QAA A   A   AA/P9                        
+                           /A    AAA  AA  AAA    AAAO   AAAe AA  AA                                 
+                              AAAAA   AA    GAAAAH  A  AA A   AAAA                                  
+                            AA        IA  AAAA AY  A    AA        AAA                               
+                         AAw           AAA A   A  AQDwAAAA           A                              
+                       AA AM                        AA             AAAB                             
+                       jwAAAAAAAgAAQAAwAAEAAAAACAAC              AAA  AB                            
+                                     hAAAA         AAIABhgAQAgABBg  AA                     '.join   
